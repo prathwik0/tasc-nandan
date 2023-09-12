cc18bb604feb4cf8406fe404d9317364b117ba9c
nandanpi <nandanrp09@gmail.com>
2023-09-13 00:17:53 +0530

    Init Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+    <>
+      <div className={'bg-black w-full h-screen'}>
+        <h1
+            className={'flex justify-center items-center w-full h-screen text-white text-6xl font-bold'}
+        >
+            TASC NMAMIT
+        </h1>
+      </div>
+    </>
+  )
+}

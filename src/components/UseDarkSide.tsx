fb3eadef0be622fcfc89d12787bffc6c7850070e
nandanpi <nandanrp09@gmail.com>
2023-09-14 22:04:39 +0530

    Deployement issue fixed

--- src/components/UseDarkSide.tsx
+++ src/components/UseDarkSide.tsx
@@ -1,16 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-type Theme = 'dark' | 'light';
-export default function UseDarkSide(): [Theme, React.Dispatch<React.SetStateAction<Theme>>] {
-    const [theme, setTheme] = useState<Theme>(localStorage.theme as Theme || 'dark');
-    const colorTheme: Theme = theme === 'dark' ? 'light' : 'dark';
+export default function useDarkSide(): [string, (theme: string) => void] {
+    const [theme, setTheme] = useState<string>(() => {
+        if (typeof window !== 'undefined') {
+            return localStorage.theme || 'dark';
+        } else {
+            return 'dark';
+        }
+    });
+
+    const colorTheme: string = theme === 'dark' ? 'light' : 'dark';
 
     useEffect(() => {
-        const root = window.document.documentElement;
-        root.classList.remove(colorTheme);
-        root.classList.add(theme);
-
-        localStorage.setItem('theme', theme);
+        if (typeof window !== 'undefined') {
+            const root = window.document.documentElement;
+            root.classList.remove(colorTheme);
+            root.classList.add(theme);
+            localStorage.setItem('theme', theme);
+        }
     }, [theme, colorTheme]);
 
     return [colorTheme, setTheme];
